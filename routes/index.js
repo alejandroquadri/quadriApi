686a5b5086e6bd393b758e4a541bb6fcdcc1f9a5
Alejandro Quadri <alejandroquadri@gmail.com>
2017-07-28 11:37:33 -0300

    commit inicial. Funciona un get a la base de datos de calipso. Esta puesto una base de pprueba

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../queries');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quadri' });
+});
+
+router.get('/api/facturaVenta', db.facturaVenta);
+
+
+module.exports = router;
