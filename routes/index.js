68351a33aa36d79dc154173615f415c1126ae667
Alejandro Quadri <alejandroquadri@gmail.com>
2017-08-07 17:54:49 -0300

    movi el archivo queries, funciona

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var db = require('../queries');
+var db = require('./queries');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quadri' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Quadri' });
+// });
 
 router.get('/api/prueba', db.prueba);
 router.get('/api/facturacion', db.facturacion);
