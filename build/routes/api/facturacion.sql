a6f0bafad85eda6ca30590b2118579eb7c7bca60
Alejandro Quadri <alejandroquadri@gmail.com>
2017-08-10 15:43:00 -0300

    .gitignore is now working

--- build/routes/api/facturacion.sql
+++
@@ -1,3 +0,0 @@
-SELECT * 
-FROM trfacturaventa 
-LIMIT ${q}
--- build/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var router = require('express').Router();
-
-console.log('llega api index reload');
-
-router.use('/ventas', require('./ventas'));
-
-module.exports = router;
