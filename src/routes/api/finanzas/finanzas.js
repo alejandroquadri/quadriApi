2e9b789d360930617dc419e40b6840b74e092a00
Alejandro Quadri <alejandroquadri@gmail.com>
2017-11-13 17:45:55 -0300

    agregada la ruta de balance con hasta

--- src/routes/api/finanzas/finanzas.js
+++ src/routes/api/finanzas/finanzas.js
@@ -26,6 +26,25 @@ router.get('/balance', function(req, res, next) {
   });
 });
 
+router.get('/balanceFecha/:fechaHasta', function(req, res, next) {
+  const today = moment();
+  let hasta = moment(req.params.fechaHasta).format('YYYYMMDD');
+  let desde = today.subtract(12, 'months').format('YYYYMMDD');
+
+  db.any(balanceSQL, {fechaDesde:desde, fechaHasta:hasta })
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved sales'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+});
+
 router.get('/resultados', function(req, res, next) {
   const today = moment();
   let hasta = today.format('YYYYMMDD');
