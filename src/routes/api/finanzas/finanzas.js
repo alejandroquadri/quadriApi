c0eaaff5aee3e9ab0240ba25a5205c161d21e23c
Alejandro Quadri <alejandroquadri@gmail.com>
2017-11-23 12:02:18 -0300

    agregadas consultas de resultadosextendida y presupuestos

--- src/routes/api/finanzas/finanzas.js
+++ src/routes/api/finanzas/finanzas.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 const balanceSQL = sql(__dirname, './balance.sql');
 const resultadosSQL = sql(__dirname, './resultados.sql');
+const resultadosExtSQL = sql(__dirname, './resultadosExtended.sql');
 
 router.get('/balance', function(req, res, next) {
   const today = moment();
@@ -29,6 +30,7 @@ router.get('/balance', function(req, res, next) {
 router.get('/balanceFecha/:fechaHasta', function(req, res, next) {
   const today = moment();
   let hasta = moment(req.params.fechaHasta).format('YYYYMMDD');
+  console.log('fecha hasta', hasta);
   let desde = today.subtract(12, 'months').format('YYYYMMDD');
 
   db.any(balanceSQL, {fechaDesde:desde, fechaHasta:hasta })
@@ -37,7 +39,7 @@ router.get('/balanceFecha/:fechaHasta', function(req, res, next) {
     .json({
       status: 'success',
       data: data,
-      message: 'Retrieved sales'
+      message: 'Retrieved balance'
     });
   })
   .catch(function (err) {
@@ -55,7 +57,25 @@ router.get('/resultados', function(req, res, next) {
       .json({
         status: 'success',
         data: data,
-        message: 'Retrieved sales'
+        message: 'Retrieved resultados'
+      });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+});
+
+router.get('/resultadosExt', function(req, res, next) {
+  const today = moment();
+  let hasta = today.format('YYYYMMDD');
+  let desde = today.subtract(12, 'months').format('YYYYMMDD');
+  db.any(resultadosExtSQL, {fechaDesde:desde, fechaHasta:hasta })
+  .then(function (data) {
+    res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved resultados ext'
       });
   })
   .catch(function (err) {
--- src/routes/api/ventas/ventas.js
+++ src/routes/api/ventas/ventas.js
@@ -4,12 +4,31 @@ import { sql } from '../../../dbHelper';
 const moment = require("moment");
 
 const facturacionSQL = sql(__dirname, './facturacion.sql');
+const psp = sql(__dirname, './presupuestos.sql');
 
 router.get('/', function(req, res, next) {
   let today = moment();
   let hasta = today.format('YYYYMMDD');
   let desde = today.subtract(6, 'months').format('YYYYMMDD');
   db.any(facturacionSQL, {fechaDesde:desde, fechaHasta:hasta })
+  .then(function (data) {
+    res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved sales'
+      });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+});
+
+router.get('/psp', function(req, res, next) {
+  let today = moment();
+  let hasta = today.format('YYYYMMDD');
+  let desde = today.subtract(12, 'months').format('YYYYMMDD');
+  db.any(psp, {fechaDesde:desde, fechaHasta:hasta })
   .then(function (data) {
     res.status(200)
       .json({
