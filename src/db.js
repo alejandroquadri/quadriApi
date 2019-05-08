b1ced80e339b03570ecc5ccbe4db88c2ffb16906
Alejandro Quadri <ale@quadri.com.ar>
2019-05-08 15:12:52 -0300

    corregido direccion de bd

--- src/db.js
+++ src/db.js
@@ -7,21 +7,21 @@ const options = {
 
 const pgp = require('pg-promise')(options);
 
-const config = {
-    host: '192.168.0.205',
-    port: 5432,
-    database: 'QP20190325',
-    user: 'calipso',
-    password: 'calipso'
-};
-
 // const config = {
 //     host: '192.168.0.205',
 //     port: 5432,
-//     database: 'QUADRIBDN',
+//     database: 'QP20190325',
 //     user: 'calipso',
 //     password: 'calipso'
 // };
+
+const config = {
+    host: '192.168.0.205',
+    port: 5432,
+    database: 'QUADRIBDN',
+    user: 'calipso',
+    password: 'calipso'
+};
 
 // con direccion de bd desde internet
 // const config = {
